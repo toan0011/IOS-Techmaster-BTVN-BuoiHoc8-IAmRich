7255f1a5377b5d8cfa8dcd8c3ed405639978f0a2
toan0011 <nmtoan.0011@gmail.com>
2023-10-26 09:40:19 +0700

    Initial Commit

---
+++ IAmRich2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich2
+//
+//  Created by Minh Toan on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
